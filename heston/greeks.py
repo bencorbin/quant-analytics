e83d1f92da9c8fe4596137aff3496bc3e32ac193
bencorbin <benjamincorbincontact@gmail.com>
2019-12-09 20:27:49 +0000

    added functionality to calculate black scholes greeks for changes in stock price

--- heston/greeks.py
+++ heston/greeks.py
@@ -1,16 +1,19 @@
 from heston.closed_form import Heston
+from black_scholes.closed_form import BlackScholes
 from numpy import arange
 import matplotlib.pyplot as plt
 
 
 class HestonGreeks:
 
-    def __init__(self, greeks, *initial_conditions):
+    def __init__(self, greeks, heston_init_conditions, blackscholes_init_conditions):
         self.greeks = greeks
 
-        (self.s0, self.v0, self.theta, self.kappa, self.sigma, self.r, self.rho, self.t, self.k) = initial_conditions
+        (self.s0, self.v0, self.theta, self.kappa, self.sigma, self.r, self.rho, self.t, self.k) = heston_init_conditions
 
-    def greek_function_mapping(self, hest, greek):
+        (self.s, self.k, self.t, self.sigma, self.r) = blackscholes_init_conditions
+
+    def hest_greek_function_mapping(self, hest, greek):
         map = {
             'delta': hest.delta(),
             'gamma': hest.gamma(),
@@ -23,19 +26,38 @@ class HestonGreeks:
 
         return map[greek]
 
+    def bs_greek_function_mapping(self, bs, greek, option_type):
+        map = {
+            'delta': bs.delta(option_type),
+            'gamma': bs.gamma(option_type),
+            'rho': bs.rho(option_type),
+            'theta': bs.theta(option_type),
+            'vega': bs.vega(option_type),
+            # 'volga': bs.volga(option_type),
+            # 'vanna': bs.vanna(option_type)
+        }
+
+        return map[greek]
+
     def greek_sim(self):
 
         for greek in self.greeks['greeks']:
-            store_greek = []
+            store_hest_greek = []
+            store_bs_greek = []
             start = self.greeks['start']
             end = self.greeks['end']
-            for stock in arange(start, end, 0.1):
+            for stock in arange(start, end, 0.5):
                 hest = Heston(stock, self.v0, self.theta, self.kappa, self.sigma, self.r, self.rho, self.t, self.k)
-                greek_value = self.greek_function_mapping(hest, greek)
-                store_greek.append(greek_value)
+                bs = BlackScholes(stock, self.k, self.t, self.sigma, self.r)
+
+                hest_greek_value = self.hest_greek_function_mapping(hest, greek)
+                bs_greek_value = self.bs_greek_function_mapping(bs, greek, 'call')
+
+                store_hest_greek.append(hest_greek_value)
+                store_bs_greek.append(bs_greek_value)
 
             plt.figure()
-            plt.plot(arange(start, end, 0.1), store_greek)
+            plt.plot(arange(start, end, 0.5), store_hest_greek, arange(start, end, 0.5), store_bs_greek)
             plt.xlabel('Stock Price')
             plt.ylabel("{0}".format(greek))
             plt.title("{0} of European Call Option".format(greek))
@@ -43,16 +65,18 @@ class HestonGreeks:
             plt.show()
 
 
-initial_conditions = [154.08, 0.0105, 0.0837, 74.32, 3.4532, 0.1, -0.8912, 1 / 365, 147]
-plot = HestonGreeks({
-    'start': 143,
-    'end': 150,
-    'greeks':
-        ['delta', 'gamma', 'rho', 'theta', 'vega', 'volga', 'vanna']
-},
-    *initial_conditions)
-# plot = HestonGreeks({'start': 143, 'end': 150, 'greeks': ['theta']},
-#                     *initial_conditions)
+hest_initial_conditions = (154.08, 0.0105, 0.0837, 74.32, 3.4532, 0.1, -0.8912, 15 / 365, 155)
+bs_initial_conditions = (154.08, 155, 15 / 365, 0.2331, 0.1)
+# plot = HestonGreeks({
+#     'start': 143,
+#     'end': 150,
+#     'greeks':
+#         ['delta', 'gamma', 'rho', 'theta', 'vega', 'volga', 'vanna']
+# },
+#     *initial_conditions)
+plot = HestonGreeks({'start': 120, 'end': 190, 'greeks': ['delta', 'gamma', 'rho', 'theta']},
+                    hest_initial_conditions,
+                    bs_initial_conditions)
 plot.greek_sim()
 
 
